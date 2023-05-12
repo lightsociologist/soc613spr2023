c002778142b0577be32000ceb064774819b8b9e1
Ryan Light <35271257+lightsociologist@users.noreply.github.com>
2023-05-11 17:45:47 -0700

    update hw6

--- homework/006hw/scripts/organizing_data.R
+++ homework/006hw/scripts/organizing_data.R
@@ -1,20 +1,27 @@
 library(igraph)
+library(readr)
+
+cross_parker_attributes <- read_csv("data/cross_parker_attributes.csv", 
+                                    col_types = cols(region = col_number(), 
+                                                    gender = col_number()))
 
 org.df <- read.table("data/cross_parker_consulting.txt", header=TRUE)
 
-orgnet <- graph_from_data_frame(ord.df)
-
-orgnet <- delete.edges(orgnet, E(orgnet)[E(orgnet)$weight < 2]) 
+orgnet <- graph_from_data_frame(org.df)
 
 #get rid of edges that are 1 for never wtf.
 
-V(orgnet)$region <- as.numeric(cross_parker_attributes$region)
+orgnet <- delete.edges(orgnet, E(orgnet)[E(orgnet)$weight < 2]) 
+
+V(orgnet)$region <- cross_parker_attributes$region
 
 V(orgnet)$gender <- as.numeric(cross_parker_attributes$gender)
 
 orgnet <- delete.vertices(orgnet, which(degree(orgnet)==0))
 
 orgnet <- simplify(orgnet)
+
+orgnet <- as.undirected(orgnet, mode="collapse")
 
 plot(orgnet, vertex.color=V(orgnet)$region)
 
